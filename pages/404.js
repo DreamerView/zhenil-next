36a3448a89189f1456e680b7f4547c0165fd2306
Dreamer <temirkhan.onyx@gmail.com>
2022-05-18 17:30:06 +0600

    404 fixed

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,13 @@
-const Error = () => {
+export default function Error() {
     return(
-        <div className="page_not_found">
-            <div className="page_not_found_block">
-                <img src="/img/404.webp" alt="not found"/>
-                <h1 className="page_not_found">We are sorry,</h1>
-                <p className="page_not_found">but the page you were looking for can't be found</p>
+        <>
+            <div className="page_not_found">
+                <div className="page_not_found_block">
+                    <img src="/img/404.webp" alt="not found"/>
+                    <h1 className="page_not_found">We are sorry,</h1>
+                    <p className="page_not_found">but the page you were looking for can not be found</p>
+                </div>
             </div>
-        </div>
+        </>
     )
 };
-
-export default Error;
