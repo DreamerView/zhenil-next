02c9f4b80dbd8c93daf3836de483fe31ffa69492
Dreamer <temirkhan.onyx@gmail.com>
2022-05-23 23:06:16 +0600

    module added

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,83 @@
 /*jshint esversion: 6 */
-import Head from 'next/head';
-import Acc from './acc/index';
+import Image from "next/image";
+import Link from "next/link";
+import Head from "next/head";
+import styles from '../styles/index_main.module.css';
 
 const Home = () => {
   return(
     <>
       <Head>
-        <title>Hello world</title>
+        <title>Okki.kz</title>
       </Head>
-      <Acc></Acc>
+      <div className={styles.main__menu_nav}>
+        <div className={styles.main__menu_nav_blocks}>
+          <Link href="/acc">
+            <a>
+              <div className={styles.main__menu_nav_block}>
+                <Image width={26} height={26} src="/img/test.svg"/>
+                <span className={styles.nav_header}>Trending</span>
+              </div>
+            </a>
+          </Link>
+          <Link href="/acc">
+            <a>
+              <div className={styles.main__menu_nav_block}>
+                <Image width={26} height={26} src="/img/test.svg"/>
+                <span className={styles.nav_header}>Trending</span>
+              </div>
+            </a>
+          </Link>
+          <Link href="/acc">
+            <a>
+              <div className={styles.main__menu_nav_block}>
+                <Image width={26} height={26} src="/img/test.svg"/>
+                <span className={styles.nav_header}>Trending</span>
+              </div>
+            </a>
+          </Link>
+          <Link href="/acc">
+            <a>
+              <div className={styles.main__menu_nav_block}>
+                <Image width={26} height={26} src="/img/test.svg"/>
+                <span className={styles.nav_header}>Trending</span>
+              </div>
+            </a>
+          </Link>
+          <Link href="/acc">
+            <a>
+              <div className={styles.main__menu_nav_block}>
+                <Image width={26} height={26} src="/img/test.svg"/>
+                <span className={styles.nav_header}>Trending</span>
+              </div>
+            </a>
+          </Link>
+          <Link href="/acc">
+            <a>
+              <div className={styles.main__menu_nav_block}>
+                <Image width={26} height={26} src="/img/test.svg"/>
+                <span className={styles.nav_header}>Trending</span>
+              </div>
+            </a>
+          </Link>
+          <Link href="/acc">
+            <a>
+              <div className={styles.main__menu_nav_block}>
+                <Image width={26} height={26} src="/img/test.svg"/>
+                <span className={styles.nav_header}>Trending</span>
+              </div>
+            </a>
+          </Link>
+          <Link href="/acc">
+            <a>
+              <div className={styles.main__menu_nav_block}>
+                <Image width={26} height={26} src="/img/test.svg"/>
+                <span className={styles.nav_header}>Trending</span>
+              </div>
+            </a>
+          </Link>
+        </div>
+      </div>
     </>
   )
 };
