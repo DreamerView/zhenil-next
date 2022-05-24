50f3f13421bc357da0664e0d5771f55b58a4a808
Dreamer <temirkhan.onyx@gmail.com>
2022-05-25 00:06:42 +0600

    bug fixed

--- pages/_document.js
+++ pages/_document.js
@@ -19,32 +19,32 @@ const DocumentShow = () => {
                 <meta name="apple-mobile-web-app-capable" content="yes" />
                 <meta name="apple-mobile-web-app-capacity" content="yes" />
                 <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-                <link rel="apple-touch-startup-image" sizes="1024x1366" href="splash_screens/12.9__iPad_Pro_landscape.png" />
-                <link rel="apple-touch-startup-image" sizes="834x1194" href="splash_screens/11__iPad_Pro__10.5__iPad_Pro_landscape.png" />
-                <link rel="apple-touch-startup-image" sizes="834x1112" href="splash_screens/10.5__iPad_Air_landscape.png" />
-                <link rel="apple-touch-startup-image" sizes="810x1080" href="splash_screens/10.2__iPad_landscape.png" />
-                <link rel="apple-touch-startup-image" sizes="768x1024" href="splash_screens/9.7__iPad_Pro__7.9__iPad_mini__9.7__iPad_Air__9.7__iPad_landscape.png" />
-                <link rel="apple-touch-startup-image" sizes="428x926" href="splash_screens/iPhone_13_Pro_Max__iPhone_12_Pro_Max_landscape.png" />
-                <link rel="apple-touch-startup-image" sizes="390x844" href="splash_screens/iPhone_13_Pro__iPhone_13__iPhone_12_Pro__iPhone_12_landscape.png" />
-                <link rel="apple-touch-startup-image" sizes="375x812" href="splash_screens/iPhone_13_mini__iPhone_12_mini__iPhone_11_Pro__iPhone_XS__iPhone_X_landscape.png" />
-                <link rel="apple-touch-startup-image" sizes="414x896" href="splash_screens/iPhone_11_Pro_Max__iPhone_XS_Max_landscape.png" />
-                <link rel="apple-touch-startup-image" sizes="414x896" href="splash_screens/iPhone_11__iPhone_XR_landscape.png" />
-                <link rel="apple-touch-startup-image" sizes="414x736" href="splash_screens/iPhone_8_Plus__iPhone_7_Plus__iPhone_6s_Plus__iPhone_6_Plus_landscape.png" />
-                <link rel="apple-touch-startup-image" sizes="375x667" href="splash_screens/iPhone_8__iPhone_7__iPhone_6s__iPhone_6__4.7__iPhone_SE_landscape.png" />
-                <link rel="apple-touch-startup-image" sizes="320x568" href="splash_screens/4__iPhone_SE__iPod_touch_5th_generation_and_later_landscape.png" />
-                <link rel="apple-touch-startup-image" sizes="1024x1366" href="splash_screens/12.9__iPad_Pro_portrait.png" />
-                <link rel="apple-touch-startup-image" sizes="834x1194" href="splash_screens/11__iPad_Pro__10.5__iPad_Pro_portrait.png" />
-                <link rel="apple-touch-startup-image" sizes="834x1112" href="splash_screens/10.5__iPad_Air_portrait.png" />
-                <link rel="apple-touch-startup-image" sizes="810x1080" href="splash_screens/10.2__iPad_portrait.png" />
-                <link rel="apple-touch-startup-image" sizes="768x1024" href="splash_screens/9.7__iPad_Pro__7.9__iPad_mini__9.7__iPad_Air__9.7__iPad_portrait.png" />
-                <link rel="apple-touch-startup-image" sizes="428x926" href="splash_screens/iPhone_13_Pro_Max__iPhone_12_Pro_Max_portrait.png" />
-                <link rel="apple-touch-startup-image" sizes="390x844" href="splash_screens/iPhone_13_Pro__iPhone_13__iPhone_12_Pro__iPhone_12_portrait.png" />
-                <link rel="apple-touch-startup-image" sizes="375x812" href="splash_screens/iPhone_13_mini__iPhone_12_mini__iPhone_11_Pro__iPhone_XS__iPhone_X_portrait.png" />
-                <link rel="apple-touch-startup-image" sizes="414x896" href="splash_screens/iPhone_11_Pro_Max__iPhone_XS_Max_portrait.png" />
-                <link rel="apple-touch-startup-image" sizes="414x896" href="splash_screens/iPhone_11__iPhone_XR_portrait.png" />
-                <link rel="apple-touch-startup-image" sizes="414x736" href="splash_screens/iPhone_8_Plus__iPhone_7_Plus__iPhone_6s_Plus__iPhone_6_Plus_portrait.png" />
-                <link rel="apple-touch-startup-image" sizes="375x667" href="splash_screens/iPhone_8__iPhone_7__iPhone_6s__iPhone_6__4.7__iPhone_SE_portrait.png" />
-                <link rel="apple-touch-startup-image" sizes="320x568" href="splash_screens/4__iPhone_SE__iPod_touch_5th_generation_and_later_portrait.png" />
+                <link rel="apple-touch-startup-image" sizes="1024x1366" href="/splash_screens/12.9__iPad_Pro_landscape.png" />
+                <link rel="apple-touch-startup-image" sizes="834x1194" href="/splash_screens/11__iPad_Pro__10.5__iPad_Pro_landscape.png" />
+                <link rel="apple-touch-startup-image" sizes="834x1112" href="/splash_screens/10.5__iPad_Air_landscape.png" />
+                <link rel="apple-touch-startup-image" sizes="810x1080" href="/splash_screens/10.2__iPad_landscape.png" />
+                <link rel="apple-touch-startup-image" sizes="768x1024" href="/splash_screens/9.7__iPad_Pro__7.9__iPad_mini__9.7__iPad_Air__9.7__iPad_landscape.png" />
+                <link rel="apple-touch-startup-image" sizes="428x926" href="/splash_screens/iPhone_13_Pro_Max__iPhone_12_Pro_Max_landscape.png" />
+                <link rel="apple-touch-startup-image" sizes="390x844" href="/splash_screens/iPhone_13_Pro__iPhone_13__iPhone_12_Pro__iPhone_12_landscape.png" />
+                <link rel="apple-touch-startup-image" sizes="375x812" href="/splash_screens/iPhone_13_mini__iPhone_12_mini__iPhone_11_Pro__iPhone_XS__iPhone_X_landscape.png" />
+                <link rel="apple-touch-startup-image" sizes="414x896" href="/splash_screens/iPhone_11_Pro_Max__iPhone_XS_Max_landscape.png" />
+                <link rel="apple-touch-startup-image" sizes="414x896" href="/splash_screens/iPhone_11__iPhone_XR_landscape.png" />
+                <link rel="apple-touch-startup-image" sizes="414x736" href="/splash_screens/iPhone_8_Plus__iPhone_7_Plus__iPhone_6s_Plus__iPhone_6_Plus_landscape.png" />
+                <link rel="apple-touch-startup-image" sizes="375x667" href="/splash_screens/iPhone_8__iPhone_7__iPhone_6s__iPhone_6__4.7__iPhone_SE_landscape.png" />
+                <link rel="apple-touch-startup-image" sizes="320x568" href="/splash_screens/4__iPhone_SE__iPod_touch_5th_generation_and_later_landscape.png" />
+                <link rel="apple-touch-startup-image" sizes="1024x1366" href="/splash_screens/12.9__iPad_Pro_portrait.png" />
+                <link rel="apple-touch-startup-image" sizes="834x1194" href="/splash_screens/11__iPad_Pro__10.5__iPad_Pro_portrait.png" />
+                <link rel="apple-touch-startup-image" sizes="834x1112" href="/splash_screens/10.5__iPad_Air_portrait.png" />
+                <link rel="apple-touch-startup-image" sizes="810x1080" href="/splash_screens/10.2__iPad_portrait.png" />
+                <link rel="apple-touch-startup-image" sizes="768x1024" href="/splash_screens/9.7__iPad_Pro__7.9__iPad_mini__9.7__iPad_Air__9.7__iPad_portrait.png" />
+                <link rel="apple-touch-startup-image" sizes="428x926" href="/splash_screens/iPhone_13_Pro_Max__iPhone_12_Pro_Max_portrait.png" />
+                <link rel="apple-touch-startup-image" sizes="390x844" href="/splash_screens/iPhone_13_Pro__iPhone_13__iPhone_12_Pro__iPhone_12_portrait.png" />
+                <link rel="apple-touch-startup-image" sizes="375x812" href="/splash_screens/iPhone_13_mini__iPhone_12_mini__iPhone_11_Pro__iPhone_XS__iPhone_X_portrait.png" />
+                <link rel="apple-touch-startup-image" sizes="414x896" href="/splash_screens/iPhone_11_Pro_Max__iPhone_XS_Max_portrait.png" />
+                <link rel="apple-touch-startup-image" sizes="414x896" href="/splash_screens/iPhone_11__iPhone_XR_portrait.png" />
+                <link rel="apple-touch-startup-image" sizes="414x736" href="/splash_screens/iPhone_8_Plus__iPhone_7_Plus__iPhone_6s_Plus__iPhone_6_Plus_portrait.png" />
+                <link rel="apple-touch-startup-image" sizes="375x667" href="/splash_screens/iPhone_8__iPhone_7__iPhone_6s__iPhone_6__4.7__iPhone_SE_portrait.png" />
+                <link rel="apple-touch-startup-image" sizes="320x568" href="/splash_screens/4__iPhone_SE__iPod_touch_5th_generation_and_later_portrait.png" />
       </Head>
       <body>
         <Main />
