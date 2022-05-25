38d41b8f644393cad66c11b8d4e5f3f3c5b74635
Dreamer <temirkhan.onyx@gmail.com>
2022-05-25 17:12:23 +0600

    config changed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+const headers = require('./headers');
+
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
