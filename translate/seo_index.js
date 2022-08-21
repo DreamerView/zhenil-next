3b856af456e26ae969e830e0fd739b1ad7c08ca9
Dreamer <temirkhan.onyx@gmail.com>
2022-08-22 03:20:04 +0600

    seo description changed

--- translate/seo_index.js
+++ translate/seo_index.js
@@ -5,8 +5,8 @@ export default {
         en:"Okki - Because simply!"
     },
     desc:{
-        kk:"Okki - бұл барлық пайдаланушыларға арналған және сіздің өміріңізді жеңілдетіп, мәселелеріңізді шешуге және уақытты азайтуға мүмкіндік беретін шағын қызметтер сайты. Сайтта өнімдердің әртүрлі түрлерін жасау мен жобалау, қажетті деректер мен құжаттарды жылдам есептеу және т.б. мүмкіндіктерді ұсынады. Сайт үнемі дамып отырады және жаңа мүмкіндіктерді қоса береді, осылайша сіздің өміріңізді жеңілдетеді.",
-        ru:"Okki - сайт мини-сервисов,предоставляющий возможности всем пользователям минимизировать время на решение ваших задач упрощая вам жизнь. Сайт предоставляет возможности создавать и конструировать различные виды продуктов, подсчитать в быстром времени необходимые вами данные и документов и т.д. Сайт постоянно развивается и добавляются новые функции тем самым упрощая вам жизнь.",
-        en:"Okki is a mini-services site that provides opportunities for all users to minimize the time to solve your problems, making your life easier. The site provides the ability to create and design various types of products, quickly calculate the data and documents you need, etc. The site is constantly evolving and new features are added, thereby simplifying your life."
+        kk:"Okki - бұл сіздің өміріңізді жеңілдетуге, мәселелеріңізді шешуге және уақытты азайтуға мүмкіндік беретін қызметтер сайты.",
+        ru:"Okki - сайт мини-сервисов, предоставляющий возможности всем пользователям минимизировать время на решение ваших задач упрощая вам жизнь.",
+        en:"Okki is a mini-services site that provides opportunities for all users to minimize the time to solve your problems, making your life easier."
     }
 }
