bfb551928aa3be14debb5cbd211afe47b6190b6b
Dreamer <temirkhan.onyx@gmail.com>
2022-08-27 19:16:19 +0600

    image compressed

--- compress-image/compress.js
+++ compress-image/compress.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 fs.readdir(testFolder, (err, files) => {
     files.forEach(file => {
-        sharp(path.resolve(__dirname,'../public/emoji/'+file)).toFormat('webp').webp({ quality: 100 }).resize(60,60).toFile(path.resolve(__dirname,'../public/emoji-small/'+file));
+        sharp(path.resolve(__dirname,'../public/emoji/'+file)).toFormat('webp').webp({ quality: 100 }).resize(160,160).toFile(path.resolve(__dirname,'../public/emoji-small/'+file));
     });
 });
 
