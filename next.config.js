2f3d61c0840ca908ac94d58389eb259013d7cc34
Dreamer <temirkhan.onyx@gmail.com>
2022-10-16 13:39:05 +0600

    s

--- next.config.js
+++ next.config.js
@@ -1,10 +1,67 @@
 /*jshint esversion: 8 */
 /** @type {import('next').NextConfig} */
-const {v4} = require('uuid');
+// const {v4} = require('uuid');
 
-const key_pass = v4();
+const key_pass = "4piP7FKREnYA+S0CcxJe360Aph9zQN5AWr1xuxjSP+o=";
 
 const production = process.env.NODE_ENV === 'production';
+
+const src = 'https://cdnjs.cloudflare.com';
+
+const secure = production?[
+      {
+        key: 'X-DNS-Prefetch-Control',
+        value: 'on'
+    },
+    {
+        key: 'Strict-Transport-Security',
+        value: 'max-age=63072000; includeSubDomains; preload'
+    },
+    {
+        key: 'Server',
+        value: 'Nginx' // phony server value
+    },
+    {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff'
+    },
+    {
+        key: 'X-Frame-Options',
+        value: 'SAMEORIGIN'
+    },
+    {
+        key: 'X-XSS-Protection',
+        value: '1; mode=block'
+    },
+    {
+        key: 'Referrer-Policy',
+        value: 'origin-when-cross-origin'
+    },
+    {
+        key: 'Permissions-Policy',
+        value: 'camera=(), microphone=(), geolocation=()'
+    },
+    {
+        key: 'X-Permitted-Cross-Domain-Policies',
+        value: 'none'
+    },
+    {
+        key: 'Cross-Origin-Opener-Policy',
+        value: "same-origin"
+    },
+    {
+        key: 'Cross-Origin-Resource-Policy',
+        value: "same-site"
+    },
+    {
+        key: 'Cross-Origin-Embedder-Policy',
+        value: "require-corp"
+    },
+    {
+        key:"Content-Security-Policy",
+        value:"script-src 'report-sample' 'self' 'strict-dynamic' 'nonce-"+key_pass+"' 'unsafe-inline'; script-src-elem 'self'  'nonce-"+key_pass+"'; script-src-attr 'self'; connect-src 'self';base-uri 'none';form-action 'self';object-src 'none'; child-src 'none';frame-src 'self';img-src 'self' data:;manifest-src 'self';prefetch-src 'self';worker-src 'self';font-src 'self';  style-src 'self' 'report-sample'; style-src-elem  'self' 'unsafe-inline'; style-src-attr 'self' 'unsafe-inline'; default-src 'self';media-src 'self';frame-ancestors 'self';upgrade-insecure-requests;"
+    }
+    ]:[{}];
 
 module.exports = {
   poweredByHeader: false,
@@ -23,60 +80,7 @@ module.exports = {
     return [
       {
         source: '/:path*',
-        headers:[
-          {
-            key: 'X-DNS-Prefetch-Control',
-            value: 'on'
-        },
-        {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=63072000; includeSubDomains; preload'
-        },
-        {
-            key: 'Server',
-            value: 'Nginx' // phony server value
-        },
-        {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff'
-        },
-        {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN'
-        },
-        {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block'
-        },
-        {
-            key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin'
-        },
-        {
-            key: 'Permissions-Policy',
-            value: 'camera=(), microphone=(), geolocation=()'
-        },
-        {
-            key: 'X-Permitted-Cross-Domain-Policies',
-            value: 'none'
-        },
-        {
-            key: 'Cross-Origin-Opener-Policy',
-            value: "same-origin"
-        },
-        {
-            key: 'Cross-Origin-Resource-Policy',
-            value: "same-site"
-        },
-        {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: "require-corp"
-        },
-        {
-            key:"Content-Security-Policy",
-            value:"script-src 'self' 'strict-dynamic' https://cdnjs.cloudflare.com; script-src-elem 'self' https://cdnjs.cloudflare.com 'nonce-4piP7FKREnYA+S0CcxJe360Aph9zQN5AWr1xuxjSP+o='; script-src-attr 'self'; connect-src 'self';base-uri 'none';form-action 'self';object-src 'none'; child-src 'none';frame-src 'self';img-src 'self' data:;manifest-src 'self';prefetch-src 'self';worker-src 'self';font-src 'self';  style-src 'self'; style-src-elem  'self' 'unsafe-inline'; style-src-attr 'self' 'unsafe-inline'; default-src 'self';media-src 'self';frame-ancestors 'self';upgrade-insecure-requests;"
-        }
-        ]
+        headers: secure
       },
     ];
   }
