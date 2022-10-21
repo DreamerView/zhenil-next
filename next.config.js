620a7204ecc7240921ef80a26ee511272b1a8284
Dreamer <temirkhan.onyx@gmail.com>
2022-10-21 22:06:40 +0600

    ssd

--- next.config.js
+++ next.config.js
@@ -11,7 +11,9 @@ const src = 'https://cdnjs.cloudflare.com';
 let build_id;
 const withPWA = require('next-pwa')({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development'
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true
 })
 
 const ContentSecurityPolicy = `
