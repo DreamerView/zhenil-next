c5b5c4c992f3077d7dd9e4c0efa3e4f1785488d1
Dreamer <temirkhan.onyx@gmail.com>
2023-01-04 11:32:27 +0600

    asdads

--- pages/signin/index.js
+++ pages/signin/index.js
@@ -56,6 +56,7 @@ const LoginForm = ({providers,data}) => {
     const router = useRouter();
     const [wait,setWait] = useState(false);
     const [passValue,setPassValue] = useState('password');
+    const [verified,setVerify] = useState(false);
     const setNotification = useCallback(({user,content,title,image}) => {
         send({
             type:"setNotification",
@@ -67,13 +68,13 @@ const LoginForm = ({providers,data}) => {
             }
         });
     },[send]);
-    const handlerLogin = async(e) =>{
+    const handlerEmail = async(e) =>{
         e.preventDefault();
         if(wait===false) {
             const aes = new AesEncryption();
             aes.setSecretKey(process.env.aesKey);
             const email = aes.encrypt(e.target[0].value);
-            const password = aes.encrypt(e.target[1].value);
+            const client = aes.encrypt("okki");
             setWait(true);
             try {
                 if (typeof window !== 'undefined') {
@@ -86,7 +87,53 @@ const LoginForm = ({providers,data}) => {
                             "Accept":"application/json; charset=utf-8",
                             "Content-Type": "application/json; charset=utf-8"
                         },
-                        body: JSON.stringify({email:email,password:password})
+                        body: JSON.stringify({email:email,client:client})
+                    };
+                    const login = await fetch(process.env.backend+"/verify-email", requestOptions);
+                    if (login.status ===404) {
+                        setNotification({user:"admin",content:"User email is not found!"});
+                        setTimeout(()=>setWait(false),[1000]);
+                    } else if(login.status ===500) {
+                        setNotification({user:"admin",content:"Something going wrong"});
+                        setTimeout(()=>setWait(false),[1000]);
+                    }
+                    const result = await login.json();
+                    if(result.success===true) {
+                        const nameResult = aes.decrypt(result.name);
+                        const surnameResult = aes.decrypt(result.surname);
+                        const avatarResult = aes.decrypt(result.avatar);
+                        const emailResult = aes.decrypt(result.email);
+                        const resultAll = {name:nameResult,surname:surnameResult,avatar:avatarResult,email:emailResult};
+                        setWait(prev=>prev=false);
+                        setVerify(prev=>prev=resultAll);
+                    }
+                }
+            } catch(e) {
+                console.log(e);
+            }
+        }
+    };
+    const handlerLogin = async(e) =>{
+        e.preventDefault();
+        if(wait===false) {
+            const aes = new AesEncryption();
+            aes.setSecretKey(process.env.aesKey);
+            const email = aes.encrypt(e.target[0].value);
+            const password = aes.encrypt(e.target[1].value);
+            const client = aes.encrypt("okki");
+            setWait(true);
+            try {
+                if (typeof window !== 'undefined') {
+                    const hostname = window.location.hostname;
+                    const requestOptions = {
+                        method: 'POST',
+                        headers: {
+                            "WWW-Authenticate": process.env.authHeader,
+                            "Proxy-Authenticate":"sdadasdsa",
+                            "Accept":"application/json; charset=utf-8",
+                            "Content-Type": "application/json; charset=utf-8"
+                        },
+                        body: JSON.stringify({email:email,password:password,client:client})
                     };
                     const login = await fetch(process.env.backend+"/signin", requestOptions);
                     if (login.status ===404) {
@@ -196,23 +243,41 @@ const LoginForm = ({providers,data}) => {
             <div className={style.login_form}>
                 <h1 className={style.head_center} onClick={()=>router.push("/signup")}>Welcome back!</h1>
                 <p className={style.text_center}>Please enter your log in details below</p>
+                {verified===false?
+                <>
                 <div>
                     {providers!==null&&providers!==undefined  && Object.values(providers).map((provider) => {
                         return (
                         <div className={style.login_sn} key={provider.name}>
                             <button className={`${style.login_sn_row} anim_hover`} onClick={() => SignInWithSN(provider.id,provider.name)}>
                                 <div className={style.login_sn_row_img_row}>
-                                <Image className={style.login_sn_row_img} width={20} height={20} src={`/social-network/${provider.name}.webp`}/></div>Sign in with {provider.name}
+                                <Image className={style.login_sn_row_img} width={20} height={20} alt={provider.name} src={`/social-network/${provider.name}.webp`}/></div>Sign in with {provider.name}
                             </button>
                         </div>
                         );
                     })}
                 </div>
                 <p className="text_center">или через электронную почту:</p>
-                <form onSubmit={(e) => handlerLogin(e)}>
+                <form onSubmit={(e) => handlerEmail(e)}>
                     <div className={style.login_row}>
                         <input type="email" name="email" className={`${style.login_input} ${style.email}`} placeholder="Email" required />
-                        {/* <div className={style.password}>
+                        <button type="submit" className={`${style.login_button} anim_hover`}>{wait===true?<div className="button__preloader"><Image width={320} height={50} alt="preloader" src="/img/button-preloader.svg"/></div>:"Continue"}</button>
+                    </div>
+                </form></>:
+                <form onSubmit={(e) => handlerLogin(e)}>
+                    {/* <p>{verified.name+" "+verified.surname+" "+verified.avatar}</p> */}
+                    <input type="email" name="email" value={verified.email} required />
+                    <div className={style.signin_auth_form}>
+                        <div className={style.signin_auth_form_image}>
+                            <Image width={50} height={50} className={style.signin_auth_form_pic} src={verified.avatar} alt="avatar" />
+                        </div>
+                        <div>
+                            <h3>{verified.name+" "+verified.surname}</h3>
+                            <p className={style.sub}>{verified.email}</p>
+                        </div>
+                    </div>
+                    <div className={style.login_row}>
+                        <div className={style.password}>
                             <div className={style.password__show_row}>
                                 <div className={style.password__show}>
                                     <Image width={24} height={24} alt="password" onClick={()=>{setPassValue(passValue==="password"?"text":"password")}} src={`/img/visibility${passValue==='password'?``:`_off`}.svg`}/>
@@ -220,10 +285,10 @@ const LoginForm = ({providers,data}) => {
                             </div>
                             <input type={passValue}  name="password" className={`${style.password_input} ${style.key}`} placeholder="Password" required/>
                         </div>
-                        <Link href="/signin/forget" className={style.text_center}>Forgot password?</Link> */}
-                        <button type="submit" className={style.login_button}>{wait===true?<div className="button__preloader"><Image width={320} height={50} alt="preloader" src="/img/button-preloader.svg"/></div>:"Continue"}</button>
+                        <Link href="/signin/forget" className={style.text_center}>Forgot password?</Link>
+                        <button type="submit" className={`${style.login_button} anim_hover`}>{wait===true?<div className="button__preloader"><Image width={320} height={50} alt="preloader" src="/img/button-preloader.svg"/></div>:"Continue"}</button>
                     </div>
-                </form>
+                </form>}
             </div>
         </div>
       </>
